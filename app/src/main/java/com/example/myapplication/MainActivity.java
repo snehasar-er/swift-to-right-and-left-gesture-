a132f37ad0e4b41639311184c57e3fb06f3767de
snehasar <snehasar@cybage.com>
2021-03-04 09:32:40 +0530

    insertdataintofirebase

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,45 +1,84 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class MainActivity extends AppCompatActivity {
-		ImageView image1,image2,image3,image4;
-		String url, url1, url2, url3;
-		Glide glide, glide1, glide2, glide3;
-
-
+	EditText name, email, contact;
+	Button insert;
+	DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("user");;
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 
 			setContentView(R.layout.activity_main);
-			image1 = findViewById(R.id.image);
-			image2 = findViewById(R.id.image1);
-			image3 = findViewById(R.id.image2);
-			image4 = findViewById(R.id.image3);
+			name = findViewById(R.id.insert_name);
+			email = findViewById(R.id.insert_email);
+			contact = findViewById(R.id.insert_contact);
+			insert = findViewById(R.id.btn_insert);
 
-			url = "https://firebasestorage.googleapis.com/v0/b/fir-574b6.appspot.com/o/kiwi.jpg?alt=media&token=cdd8301f-437b-4ce3-b8ac-b05178a65cbd";
-			glide.with(getApplicationContext()).load(url).into(image1);
+			insert.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					String NAME = name.getText().toString().trim();
+					String EMAIL = email.getText().toString().trim();
+					String CONTACT = contact.getText().toString().trim();
 
-			url1 = "https://firebasestorage.googleapis.com/v0/b/fir-574b6.appspot.com/o/apples.jpg?alt=media&token=cca83591-1c37-48e3-9be1-13af0e1aec94";
-			glide1.with(getApplicationContext()).load(url1).into(image2);
+					if (NAME.isEmpty()) {
+						name.setError("Name is required");
+						name.requestFocus();
+						return;
+					}
 
-			url2 = "https://firebasestorage.googleapis.com/v0/b/fir-574b6.appspot.com/o/banana.jpg?alt=media&token=33e90898-273b-4b4e-8cb8-283ca3cc8b43";
-			glide2.with(getApplicationContext()).load(url2).into(image3);
+					if (EMAIL.isEmpty()) {
+						email.setError("Email is required");
+						email.requestFocus();
+						return;
+					}
 
-			url3 = "https://firebasestorage.googleapis.com/v0/b/fir-574b6.appspot.com/o/dragon.jpg?alt=media&token=eed581fc-f343-4b49-bd03-44f13acda4b4";
-			glide3.with(getApplicationContext()).load(url3).into(image4);
+					if (CONTACT.isEmpty()) {
+						contact.setError("Contact is required");
+						contact.requestFocus();
+						return;
+					}
+					String ID = databaseReference.push().getKey();
+					Model model = new Model(ID, NAME, EMAIL, CONTACT);
+
+					databaseReference.child(ID).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+						@Override
+						public void onComplete(@NonNull Task<Void> task) {
+							if (task.isSuccessful()) {
+								Toast.makeText(getApplicationContext(), "User Added", Toast.LENGTH_LONG).show();
+							}
+						}
+					}).addOnFailureListener(new OnFailureListener() {
+						@Override
+						public void onFailure(@NonNull Exception e) {
+							Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+						}
+					});
+				}
+
+
+			});
+
 
 		}
-
-
-
-
-
 
 	}
 
---
+++ app/src/main/java/com/example/myapplication/Model.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+
+	public class Model {
+		String id;
+		String name;
+		String email;
+		String contact;
+
+		public Model() {
+		}
+
+		public Model(String id, String name, String email, String contact) {
+			this.id = id;
+			this.name = name;
+			this.email = email;
+			this.contact = contact;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getContact() {
+			return contact;
+		}
+
+		public void setContact(String contact) {
+			this.contact = contact;
+		}
+	}
+
