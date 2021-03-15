9c79dcdcb0e4673ed6558fbdceef71366718b3e5
snehasar <snehasar@cybage.com>
2021-03-15 11:34:00 +0530

    switch_on_off

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,48 +23,30 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
-	RadioButton android, java, angular, python;
+	private Switch sw1,sw2;
+	private  Button btnGet;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		android = (RadioButton)findViewById(R.id.rdbAndroid);
-		angular = (RadioButton)findViewById(R.id.rdbAngular);
-		java = (RadioButton)findViewById(R.id.rdbJava);
-		python = (RadioButton)findViewById(R.id.rdbPython);
-		Button btn = (Button)findViewById(R.id.getBtn);
-		btn.setOnClickListener(new View.OnClickListener() {
+		sw1 = (Switch)findViewById(R.id.switch1);
+		sw2 = (Switch)findViewById(R.id.switch2);
+		btnGet = (Button)findViewById(R.id.getBtn);
+		btnGet.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String result = "Selected Course: ";
-				result+= (android.isChecked())?"Android":(angular.isChecked())?"AngularJS":(java.isChecked())?"Java":(python.isChecked())?"Python":"";
-				Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+				String str1, str2;
+				if (sw1.isChecked())
+					str1 = sw1.getTextOn().toString();
+				else
+					str1 = sw1.getTextOff().toString();
+				if (sw2.isChecked())
+					str2 = sw2.getTextOn().toString();
+				else
+					str2 = sw2.getTextOff().toString();
+				Toast.makeText(getApplicationContext(), "Switch1 -  " + str1 + " \n" + "Switch2 - " + str2,Toast.LENGTH_SHORT).show();
 			}
 		});
-	}
-	public void onRadioButtonClicked(View view) {
-		boolean checked = ((RadioButton) view).isChecked();
-		String str="";
-		// Check which radio button was clicked
-		switch(view.getId()) {
-			case R.id.rdbAndroid:
-				if(checked)
-					str = "Android Selected";
-				break;
-			case R.id.rdbAngular:
-				if(checked)
-					str = "AngularJS Selected";
-				break;
-			case R.id.rdbJava:
-				if(checked)
-					str = "Java Selected";
-				break;
-			case R.id.rdbPython:
-				if(checked)
-					str = "Python Selected";
-				break;
-		}
-		Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
 	}
 	}
 
