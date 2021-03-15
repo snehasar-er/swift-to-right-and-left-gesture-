b924e7430fc81c0361dcf08d98cfbfedcc3ed69f
snehasar <snehasar@cybage.com>
2021-03-15 12:06:41 +0530

    progressbar_loading

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,69 +1,41 @@
 package com.example.myapplication;
 
-import android.app.DatePickerDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.text.InputType;
-import android.util.Log;
+
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.EditText;
 
-import android.widget.ImageButton;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextClock;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
+	private ProgressBar pgsBar;
+	private Button showbtn, hidebtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button btnStatus = (Button)findViewById(R.id.btnCheck);
-		btnStatus.setOnClickListener(new View.OnClickListener() {
+		pgsBar = (ProgressBar) findViewById(R.id.pBar);
+		showbtn = (Button)findViewById(R.id.btnShow);
+		hidebtn = (Button)findViewById(R.id.btnHide);
+		showbtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// Check for Internet Connection
-				if (isConnected()) {
-					Toast.makeText(getApplicationContext(), "Internet Connected", Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_SHORT).show();
-				}
+				pgsBar.setVisibility(v.VISIBLE);
 			}
 		});
-	}
-	public boolean isConnected() {
-		boolean connected = false;
-		try {
-			ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-			NetworkInfo nInfo = cm.getActiveNetworkInfo();
-			connected = nInfo != null && nInfo.isAvailable() && nInfo.isConnected();
-			return connected;
-		} catch (Exception e) {
-			Log.e("Connectivity Exception", e.getMessage());
-		}
-		return connected;
+		hidebtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				pgsBar.setVisibility(v.GONE);
+			}
+		});
 	}
 }
 
