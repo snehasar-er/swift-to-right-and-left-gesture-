f8ff385827650f9f84c61afdb5824a87b0d6111a
snehasar <snehasar@cybage.com>
2021-03-15 11:38:34 +0530

    spinner

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -20,34 +23,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-	private Switch sw1,sw2;
-	private  Button btnGet;
+	String[] users = { "Suresh Dasari", "Trishika Dasari", "Rohini Alavala", "Praveen Kumar", "Madhav Sai" };
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		sw1 = (Switch)findViewById(R.id.switch1);
-		sw2 = (Switch)findViewById(R.id.switch2);
-		btnGet = (Button)findViewById(R.id.getBtn);
-		btnGet.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				String str1, str2;
-				if (sw1.isChecked())
-					str1 = sw1.getTextOn().toString();
-				else
-					str1 = sw1.getTextOff().toString();
-				if (sw2.isChecked())
-					str2 = sw2.getTextOn().toString();
-				else
-					str2 = sw2.getTextOff().toString();
-				Toast.makeText(getApplicationContext(), "Switch1 -  " + str1 + " \n" + "Switch2 - " + str2,Toast.LENGTH_SHORT).show();
-			}
-		});
+		Spinner spin = (Spinner) findViewById(R.id.spinner1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(adapter);
+		spin.setOnItemSelectedListener(this);
 	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		Toast.makeText(getApplicationContext(), "Selected User: "+users[position] ,Toast.LENGTH_SHORT).show();
 	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+
+	}
+}
 
 
